290607b9132907dfaa27b5fcf3b69f80785d7a3e
Kyle Ellrott <kellrott@gmail.com>
2015-09-03 11:23:51 -0700

    Adding vcf filter tool

---
+++ tools/tcgavcf-tool/vcf-filter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+if __name__ == "__main__":
+    allowed_seq = {}
+
+    input_vcf = sys.argv[1]
+    filter_file = sys.argv[2]
+    output_vcf = sys.argv[3]
+
+    with open(filter_file) as handle:
+        for line in handle:
+            for elem in line.rstrip().split(","):
+                allowed_seq[elem] = True
+
+    with open(input_vcf) as ihandle:
+        for open(output_vcf, "w") as ohandle:
+            for line in ihandle:
+                write = False
+                if line.start("#"):
+                    write = True
+                else:
+                    tmp = line.split("\t")
+                    if tmp[0] in allowed_seq:
+                        write = False
+                if write:
+                    ohandle.write(line)
