34a711217f5fc2acd477fc13df73d1f2ceb4d1c1
Jaclyn Smith <jacmarjorie@gmail.com>
2015-09-09 21:07:27 -0700

    edits to reheader and configs to produced center and sample configs

--- tools/tcgavcf-tool/tcga-vcf-reheader.py
+++ tools/tcgavcf-tool/tcga-vcf-reheader.py
@@ -12,20 +12,97 @@ import sys
 
 import yaml
 import datetime
+import itertools
 
 
 __version__ = '1.0.0'
+
+# these are the new params
+# {'config': {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", Platform="{platform}", 
+# Source="dbGAP",Accession="dbGaP", softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan', 
+# 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 'software_version': '2.4.0'}, 
+# 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, 
+# InputVCFSource=<varscan>,InputVCFVer=<4.1>, InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}}
+
+# key config
+# value {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", 
+# Platform="{platform}", Source="dbGAP",Accession="dbGaP", softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan', 
+# 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 'software_version': '2.4.0'}, 
+# 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, 
+# InputVCFSource=<varscan>,InputVCFVer=<4.1>, InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}
+
+# this is args.parameter {}
+# not a duplicate
+# {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", Platform="{platform}", Source="dbGAP",Accession="dbGaP", 
+# softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan', 
+# 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 'software_version': '2.4.0'}, 
+# 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, 
+# InputVCFSource=<varscan>,InputVCFVer=<4.1>, InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}
+
+# these are the new params
+# {'samples': {'TUMOR': {'aliquot_uuid': 'f23b3d0d-26a5-4adf-8aec-4994d094465b', 'aliquot_barcode': 'TCGA-W5-AA33-01A-11D-A417-09', 'platform': 'Illumina', 'analysis_uuid': 'cd5d8895-6b13-450f-993b-bff9943dc0d9', 
+# 'description': 'Primary Tumor', 'bam_name': '9a6ebf433eb4bcb93be593f74ffa1d3b.bam'}, 'NORMAL': {'aliquot_uuid': '02e2d8b9-8b5a-4bae-8615-76c46d68f44c', 'aliquot_barcode': 'TCGA-W5-AA33-10A-01D-A41A-09', 'platform': 'Illumina', 
+# 'analysis_uuid': '3118c963-8446-4d4a-8146-6d46f1465780', 'reference_genome': 'hg19', 'description': 'Normal sample', 'bam_name': '741377430d1d6a7a567f5425abc41ac2.bam'}}, 'config': {'fixed_headers': [['fileformat', False, 'VCFv4.1'], 
+# ['tcgaversion', False, '1.1'], ['phasing', False, 'none'], ['reference', False, 'hg19']]}}
+
+# key samples
+# value {'TUMOR': {'aliquot_uuid': 'f23b3d0d-26a5-4adf-8aec-4994d094465b', 'aliquot_barcode': 'TCGA-W5-AA33-01A-11D-A417-09', 'platform': 'Illumina', 'analysis_uuid': 'cd5d8895-6b13-450f-993b-bff9943dc0d9', 
+# 'description': 'Primary Tumor', 'bam_name': '9a6ebf433eb4bcb93be593f74ffa1d3b.bam'}, 'NORMAL': {'aliquot_uuid': '02e2d8b9-8b5a-4bae-8615-76c46d68f44c', 'aliquot_barcode': 'TCGA-W5-AA33-10A-01D-A41A-09', 
+# 'platform': 'Illumina', 'analysis_uuid': '3118c963-8446-4d4a-8146-6d46f1465780', 'reference_genome': 'hg19', 'description': 'Normal sample', 'bam_name': '741377430d1d6a7a567f5425abc41ac2.bam'}}
+
+# this is args.parameter {'config': {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", 
+# Platform="{platform}", Source="dbGAP",Accession="dbGaP", softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 
+# 'fixed_sample_params': {'software_name': 'varscan', 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"',
+ # 'software_version': '2.4.0'}, 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, 
+ # '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, InputVCFSource=<varscan>,InputVCFVer=<4.1>, 
+ # InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}}
+
+# not a duplicate
+# {'TUMOR': {'aliquot_uuid': 'f23b3d0d-26a5-4adf-8aec-4994d094465b', 'aliquot_barcode': 'TCGA-W5-AA33-01A-11D-A417-09', 'platform': 'Illumina', 'analysis_uuid': 'cd5d8895-6b13-450f-993b-bff9943dc0d9', 
+# 'description': 'Primary Tumor', 'bam_name': '9a6ebf433eb4bcb93be593f74ffa1d3b.bam'}, 'NORMAL': {'aliquot_uuid': '02e2d8b9-8b5a-4bae-8615-76c46d68f44c', 'aliquot_barcode': 'TCGA-W5-AA33-10A-01D-A41A-09', 
+# 'platform': 'Illumina', 'analysis_uuid': '3118c963-8446-4d4a-8146-6d46f1465780', 'reference_genome': 'hg19', 'description': 'Normal sample', 'bam_name': '741377430d1d6a7a567f5425abc41ac2.bam'}}
+
+# key config
+# value {'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['phasing', False, 'none'], ['reference', False, 'hg19']]}
+# this is args.parameter {'samples': {'TUMOR': {'aliquot_uuid': 'f23b3d0d-26a5-4adf-8aec-4994d094465b', 'aliquot_barcode': 'TCGA-W5-AA33-01A-11D-A417-09', 'platform': 'Illumina', 'analysis_uuid': 'cd5d8895-6b13-450f-993b-bff9943dc0d9', 
+# 'description': 'Primary Tumor', 'bam_name': '9a6ebf433eb4bcb93be593f74ffa1d3b.bam'}, 'NORMAL': {'aliquot_uuid': '02e2d8b9-8b5a-4bae-8615-76c46d68f44c', 'aliquot_barcode': 'TCGA-W5-AA33-10A-01D-A41A-09', 
+# 'platform': 'Illumina', 'analysis_uuid': '3118c963-8446-4d4a-8146-6d46f1465780', 'reference_genome': 'hg19', 'description': 'Normal sample', 'bam_name': '741377430d1d6a7a567f5425abc41ac2.bam'}}, 
+# 'config': {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", Platform="{platform}", 
+# Source="dbGAP",Accession="dbGaP", softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan',
+ # 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 'software_version': '2.4.0'}, 
+ # 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, 
+ # InputVCFSource=<varscan>,InputVCFVer=<4.1>, InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}}
+
+# config is a duplicate
+# before the merge occurs
+# {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", Platform="{platform}", Source="dbGAP",Accession="dbGaP", 
+# softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan', 
+# 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 
+# 'software_version': '2.4.0'}, 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['center', False, '"wustl.edu"'], ['phasing', False, 'none'], ['vcfProcessLog', False, 
+# '<InputVCF=</home/jac/galaxy/galaxy_og/database/files/000/dataset_309.dat>, InputVCFSource=<varscan>,InputVCFVer=<4.1>, 
+# InputVCFParam=<"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05">>']]}
+
+#  after the merge occurs
+# {'sample_line_format': 'SAMPLE=< ID={id}, Description="{description}", SampleUUID={aliquot_uuid},SampleTCGABarcode={aliquot_barcode}, AnalysisUUID={analysis_uuid},File="{bam_name}", Platform="{platform}", 
+# Source="dbGAP",Accession="dbGaP", softwareName=<{software_name}>, softwareVer=<{software_version}>, softwareParam=<{software_params}> >', 'fixed_sample_params': {'software_name': 'varscan', 
+# 'software_params': '"min-coverage=8,min-coverage-normal=8,min-coverage-tumor=6,min-var-freq=0.1,min-freq-for-hom=0.75,normal-purity=1.0,tumor-purity=1.0,p-value=0.99,somatic-p-value=0.05"', 'software_version': '2.4.0'},
+ # 'fixed_headers': [['fileformat', False, 'VCFv4.1'], ['tcgaversion', False, '1.1'], ['phasing', False, 'none'], ['reference', False, 'hg19']]} after the merge occurs
+
 
 
 def main():
     args = parse_args()
     args.parameter_map = {}
+    # define center and sample specific parameters
     for path in args.parameter_file_path:
         with open(path) as yaml_file:
             new_params = yaml.load(yaml_file)
             for k,v in new_params.items():
                 if k in args.parameter_map:
-                    args.parameter_map[k] = dict(args.parameter_map[k], **v)
+                    for key in args.parameter_map[k]:
+                        if key in v:
+                            args.parameter_map[k][key] = list(itertools.chain(args.parameter_map[k][key], v[key]))
+                    # args.parameter_map[k] = dict(args.parameter_map[k], **v)
                 else:
                     args.parameter_map[k] = v
     # TODO: Configure logging
@@ -58,9 +135,9 @@ def run(args):
 
 
 def write_fixed_headers(fout, fixed_headers):
+    # append fileDate
+    fixed_headers.append(['fileDate', False, datetime.date.today().strftime('%Y%m%d')])
     for name, ignored, value in fixed_headers:
-        if name == 'fileDate':
-            value = datetime.date.today().strftime('%Y%m%d')
         write_meta_line(fout, name, value)
 
 
